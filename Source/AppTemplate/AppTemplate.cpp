9b6a8edf4e8d4791f04b1993c15171b86a90136d
ponyportal2 <vorotasibiry@gmail.com>
2020-03-04 22:37:28 +0700

    hobaba

--- Source/AppTemplate/AppTemplate.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "AppTemplate.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AppTemplate, "AppTemplate" );
